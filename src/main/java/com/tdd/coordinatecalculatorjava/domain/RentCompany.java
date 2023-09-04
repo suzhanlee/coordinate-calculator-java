053714d4452ddf77ddf4a3e05195cdcf6a7998cf
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2023-09-04 20:59:41 +0900

    test : 연료 계산기 구현

---
+++ src/main/java/com/tdd/coordinatecalculatorjava/domain/RentCompany.java
@@ -0,0 +1,41 @@
+package com.tdd.coordinatecalculatorjava.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class RentCompany {
+
+    private List<Car> cars;
+
+    public static RentCompany create() {
+        RentCompany rentCompany = new RentCompany();
+        rentCompany.cars = new ArrayList<>();
+        return rentCompany;
+    }
+
+    public void addCar(Car car) {
+        cars.add(car);
+    }
+
+    public String generateReport() {
+        List<Double> chargeQuantityList = cars.stream().map(Car::getChargeQuantity).collect(
+            Collectors.toList());
+
+        StringBuilder report = new StringBuilder();
+
+        for (int i = 0; i < cars.size(); i++) {
+            Car car = cars.get(i);
+            double doubleChargeQuantity = chargeQuantityList.get(i);
+            int chargeQuantity = (int) doubleChargeQuantity;
+            report.append(car.getName()).append(" : ")
+                .append(chargeQuantity).append("리터")
+                .append('\n')
+                .append(" ");
+
+
+        }
+
+        return report.toString();
+    }
+}
---
+++ src/test/java/com/tdd/coordinatecalculatorjava/domain/CarTest.java
@@ -0,0 +1,36 @@
+package com.tdd.coordinatecalculatorjava.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CarTest {
+
+    private Car car;
+
+    @BeforeEach
+    void setUp() {
+        car = new K5(130);
+    }
+
+    @Test
+    void getDistancePerLiter() {
+        assertThat(car.getDistancePerLiter()).isEqualTo(13);
+    }
+
+    @Test
+    void getTripDistance() {
+        assertThat(car.getTripDistance()).isEqualTo(130);
+    }
+
+    @Test
+    void getName() {
+        assertThat(car.getName()).isEqualTo("K5");
+    }
+
+    @Test
+    void getChargeQuantity() {
+        assertThat(car.getChargeQuantity()).isEqualTo(10);
+    }
+}
